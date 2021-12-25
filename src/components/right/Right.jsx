1af4ef8ce25043b4cf02077f66e3711f3bc0f639
surya0180 <teja8551@gmail.com>
2021-12-25 21:24:23 +0530

    completed right part

--- src/components/right/Right.jsx
+++ src/components/right/Right.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import classes from './Right.module.css'
+import banner from '../../assets/images/undraw_creativity_re_8grt 1.png'
 
 const Right = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className={"container " + classes.header}>
+                <button className={'btn shadow-none ' + classes.loginbtn}>Login</button>
+                <button className={'btn btn-primary shadow-none ' + classes.signupbtn}>Signup</button>
+            </div>
+            <div className={"container"}>
+                <img src={banner} alt="banner" className={classes.image} />
+            </div>   
+        </>
     )
 }
 
