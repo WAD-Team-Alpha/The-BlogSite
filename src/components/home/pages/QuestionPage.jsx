5bbdc8969e532761e76b6924c9be85338b7ab68b
satyamsingh-sg <singhsatyamsg@gmail.com>
2021-12-30 19:42:06 +0530

    page routing

---
+++ src/components/home/pages/QuestionPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Question from '../middle/Question'
+const QuestionPage=()=>{
+    return (
+       <div>
+            <Question/> 
+       </div>
+      );
+}
+export default QuestionPage;
---
+++ src/components/home/pages/SavedForLaterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductCard from '../middle/productcard'
+import Question from '../middle/question'
+const SavedForLaterPage=()=>{
+    return (
+       <div>
+           <ProductCard/> 
+           <Question/>
+       </div>
+      );
+}
+export default SavedForLaterPage;
