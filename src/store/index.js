aa15ad50c38a62cc93a22531b11997514f5d75a8
bannu0snake <mahaboob.developer@gmail.com>
2022-01-05 22:58:12 +0530

    added data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth";
 import profileReducer from "./profile";
-import  counterReducer  from "./counterReducer";
-import postFormReducer from "./postForm";
-
+import counterReducer from "./counterReducer";
 const store = configureStore({
-  reducer: { auth: authReducer, profile: profileReducer, counter: counterReducer, postForm: postFormReducer},
+  reducer: {
+    auth: authReducer,
+    profile: profileReducer,
+    counter: counterReducer,
+  },
 });
 
 export default store;
