11b9a275b2799412126b1783966675afa55516ba
bannu0snake <mahaboob.developer@gmail.com>
2022-01-06 18:39:08 +0530

    completed 50% of frontend for post screen

--- src/store/post-actions.js
+++ src/store/post-actions.js
@@ -47,7 +47,7 @@ export const sendPostData = ( postData, postId ) => {
 export const fetchPostData = ( postId ) => {
 
     return async ( dispatch ) => {
-        postId = "2db7a6cb-8ce5-468f-901e-a4930366ca85";
+        postId = "1525768a-3c49-4169-8b69-98bbdaf6a287";
         console.log( "fetch data action is triggered" );
         const url = `https://fsd-project-e2e42-default-rtdb.firebaseio.com/posts/${ postId }.json`;
         const fetchData = async () => {
@@ -79,7 +79,7 @@ export const fetchPostData = ( postId ) => {
 
             }
             dispatch( postActions.add( data ) );
-            return "success";
+            return data;
 
         } catch ( error )
         {
