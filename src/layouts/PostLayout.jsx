ac551da3e4717e5bce8d59362418539dc9e6dc74
bannu0snake <mahaboob.developer@gmail.com>
2022-01-07 10:12:47 +0530

    added comments logic

--- src/layouts/PostLayout.jsx
+++ src/layouts/PostLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState,useEffect,useRef } from 'react'
 import Header from '../components/header/Header'
 import classes from './Layout.module.css'
 import Footer from '../components/footer/Footer'
@@ -9,11 +9,12 @@ import Leftp from '../components/post_details/leftp/leftp/leftp'
 import Middlep from '../components/post_details/leftp/middlep/middlep'
 import { useParams } from 'react-router-dom'
 import {motion} from 'framer-motion'
-import { useDispatch } from 'react-redux'
-import { fetchPostData } from '../store/post-actions'
+import { useDispatch,useSelector } from 'react-redux'
+import { fetchPostData, sendPostData } from '../store/post-actions'
 import { fetchOtherProfileData } from '../store/profile-actions'
 const PostLayout = () => {
     const dispatch = useDispatch();
+    const postData = useSelector((state) => state.post);
     const [nav, setNav] = useState(false);
     const [data, setData] = useState({});
     
@@ -53,6 +54,7 @@ const PostLayout = () => {
             }
         });
     }, []);
+    
 
     return (
         <>
