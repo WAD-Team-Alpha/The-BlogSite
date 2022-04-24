10ed05c24be6fc64dc89da7d264303533789314b
prathyush11 <sprathyush141@gmail.com>
2022-04-24 17:13:15 +0530

    Added user post and ques detail page frontend in admin page

--- src/admin/Posts.jsx
+++ src/admin/Posts.jsx
@@ -3,11 +3,13 @@ import classes from "./css/posts.module.css";
 import { Pagination } from "@mui/material";
 import { getPostsData } from "../requests/admin.requests";
 import LoadingSpinner from "../components/auth/LoadingSpinner";
+import { useNavigate } from "react-router-dom";
 const Posts = () => {
     // const titles = ["title1", "title2", "title3", "Minecraft", "CrabGame"]
     // const summary = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const navigate = useNavigate();
     const handleChange = async (e,value) => {
       setLoading(true)
       const data1 = await getPostsData(value);
@@ -54,9 +56,9 @@ const Posts = () => {
                                     {post.summary.slice(0, 80)} ...{" "}
                                 </p>
                             </p>
-                            <a href="#" class="btn btn-primary">
-                                View Post
-                            </a>
+                            <button class="btn btn-primary" onClick={()=>navigate("/admin/posts/id")}>
+                              View Post
+                            </button>
                         </div>
                     </div>
                 ))}
--- src/admin/Questions.jsx
+++ src/admin/Questions.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import QuestionCard from "./QuestionCard";
 import classes from "./css/questions.module.css";
 import { Pagination } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { getQuestionsData } from "../requests/admin.requests";
 import LoadingSpinner from "../components/auth/LoadingSpinner";
 const Questions = () => {
@@ -11,6 +11,7 @@ const Questions = () => {
     //   "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const navigate = useNavigate();
     const handleChange = async (e,value) => {
         setLoading(true);
         const data1 = await getQuestionsData(value);
@@ -59,12 +60,15 @@ const Questions = () => {
                                     {question.summary.slice(0, 80)} ...{" "}
                                 </p>
                             </p>
-                            <Link
+                            {/* <Link
                                 to={`/admin/questions/id`}
                                 className={classes.link}
                             >
                                 <b>View Question</b>
-                            </Link>
+                            </Link> */}
+                            <button class="btn btn-primary" onClick={()=>navigate("/admin/questions/id")} >
+                              View Question
+                            </button>
                         </div>
                     </div>
                 ))}
